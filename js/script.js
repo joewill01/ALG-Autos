f6d2b396621e0884135a8419588de7cea87b7756
joewill01 <joew4119@gmail.com>
2020-01-24 13:28:10 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  5000);
+
 $(window).on("scroll", function() {
     if($(window).scrollTop() > 10) {
         $(".brand").addClass("brand-scroll");
