ddb6ecd10069c490646d549f0c09b6e291309a21
Joe Williams <joew4119@gmail.com>
2020-01-27 14:22:59 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-$("#slideshow > div:gt(0)").hide();
+$("#slideshow > .slide:gt(0)").hide();
 
 setInterval(function() {
-  $('#slideshow > div:first')
+  $('#slideshow > .slide:first')
     .fadeOut(1000)
     .next()
     .fadeIn(1000)
     .end()
     .appendTo('#slideshow');
-},  5000);
+},  2000);
 
 $(window).on("scroll", function() {
     if($(window).scrollTop() > 10) {
         $(".brand").addClass("brand-scroll");
         $(".navbar").addClass("navbar-scroll");
+        $(".nav").addClass("nav-scroll");
     } else if($(window).scrollTop() <= 10) {
         $(".brand").removeClass("brand-scroll");
         $(".navbar").removeClass("navbar-scroll");
+        $(".nav").removeClass("nav-scroll");
     }
 });
