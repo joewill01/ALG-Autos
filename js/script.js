0bc6629a32768e3d3256c875c61ad3cac036a400
joewill01 <joew4119@gmail.com>
2020-02-24 14:28:25 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,40 +1,107 @@
 let last_header_animation = 'top';
 
-$("#slideshow > .slide:gt(0)").hide();
+/* Slideshow */
 
-setInterval(function() {
-  $('#slideshow > .slide:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  2000);
+let current_slide = 1;
+let slide_count = 5;
+
+function moveSlide() {
+    if (current_slide === 1) {
+        document.getElementById('item1').classList.add("left");
+        document.getElementById('item2').classList.add("right");
+    } else if (current_slide === 2) {
+        document.getElementById('item2').classList.add("left");
+        document.getElementById('item3').classList.add("right");
+    } else if (current_slide === 3) {
+        document.getElementById('item3').classList.add("left");
+        document.getElementById('item4').classList.add("right");
+    } else if (current_slide === 4) {
+        document.getElementById('item4').classList.add("left");
+        document.getElementById('item5').classList.add("right");
+    } else if (current_slide === 5) {
+        document.getElementById('item5').classList.add("left");
+        document.getElementById('item1').classList.add("right");
+    }
+    if (current_slide === slide_count) {
+        current_slide = 1;
+    } else {
+        current_slide++
+    }
+}
+
+window.onload = function() {
+    document.getElementById('item1').addEventListener('transitionend', () => {
+        document.getElementById('item1').classList.remove("left");
+        document.getElementById('item1').classList.remove("right");
+        if (current_slide === 1) {
+            document.getElementById('item1').classList.add("active");
+        } else {
+            document.getElementById('item1').classList.remove("active");
+        }
+    })
+
+    document.getElementById('item2').addEventListener('transitionend', () => {
+        document.getElementById('item2').classList.remove("left");
+        document.getElementById('item2').classList.remove("right");
+        if (current_slide === 2) {
+            document.getElementById('item2').classList.add("active");
+        } else {
+            document.getElementById('item2').classList.remove("active");
+        }
+    })
+
+    document.getElementById('item3').addEventListener('transitionend', () => {
+        document.getElementById('item3').classList.remove("left");
+        document.getElementById('item3').classList.remove("right");
+        if (current_slide === 3) {
+            document.getElementById('item3').classList.add("active");
+        } else {
+            document.getElementById('item3').classList.remove("active");
+        }
+    })
+
+    document.getElementById('item4').addEventListener('transitionend', () => {
+        document.getElementById('item4').classList.remove("left");
+        document.getElementById('item4').classList.remove("right");
+        if (current_slide === 4) {
+            document.getElementById('item4').classList.add("active");
+        } else {
+            document.getElementById('item4').classList.remove("active");
+        }
+    })
+
+    document.getElementById('item5').addEventListener('transitionend', () => {
+        document.getElementById('item5').classList.remove("left");
+        document.getElementById('item5').classList.remove("right");
+        if (current_slide === 5) {
+            document.getElementById('item5').classList.add("active");
+        } else {
+            document.getElementById('item5').classList.remove("active");
+        }
+    })
+
+ setInterval(moveSlide, 5000);
+}
+
+/* End slideshow */
 
 function animate_header(type) {
     if ((type === "scroll") && (last_header_animation === "top")) {
-
         anime({
             targets: '#header-svg path',
             d: 'm0 0v70q250 0 500 0v-70z',
             easing: 'easeInOutQuad',
             duration: 250,
         });
-
-
-
         last_header_animation = 'scroll';
-
-    } else if ((type === "top") && (last_header_animation === "scroll")) {
-
+    }
+    else if ((type === "top") && (last_header_animation === "scroll")) {
         anime({
             targets: '#header-svg path',
             d: 'm0 0v70q250 80 500 0v-70z',
             easing: 'easeInOutQuad',
             duration: 250,
         });
-
-
         last_header_animation = 'top';
     }
 }
